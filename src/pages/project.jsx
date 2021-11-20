f6041e6f69c647a0446fe872763c08510ef7d332
Goitsemang-cloud <63322721+Goitsemang-cloud@users.noreply.github.com>
2021-11-20 22:38:16 +0200

    mee

--- src/pages/project.jsx
+++ src/pages/project.jsx
@@ -7,9 +7,39 @@ export default function Project() {
       <h3>project's</h3>
       <div className="_projectContainter">
         <div className="_projecttitle">
-          <h4>react video chat app </h4>
+          <h4>React video chat app </h4>
         </div>
-        <div className="_projectdetails"></div>
+        <div className="_projectdetails">
+          <h3>Description</h3>
+          <p>A React application using express and socket.io</p>
+
+          <h3>Technologies used</h3>
+          <p> React js, Express js, socket io</p>
+        </div>
+      </div>
+      <div className="_projectContainter">
+        <div className="_projecttitle">
+          <h4>React-blog </h4>
+        </div>
+        <div className="_projectdetails">
+          <h3>Description</h3>
+          <p>A React application using express</p>
+
+          <h3>Technologies used</h3>
+          <p> React js, Django rest APi</p>
+        </div>
+      </div>
+      <div className="_projectContainter">
+        <div className="_projecttitle">
+          <h4>React chat App </h4>
+        </div>
+        <div className="_projectdetails">
+          <h3>Description</h3>
+          <p>A React application</p>
+
+          <h3>Technologies used</h3>
+          <p> React js, Django rest APi</p>
+        </div>
       </div>
     </div>
   );
