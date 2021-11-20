82e684c7e9a5454f30334503123ad23e55a4e7d8
Goitsemang-cloud <63322721+Goitsemang-cloud@users.noreply.github.com>
2021-11-20 21:11:50 +0200

    'second commit' commit on master

---
+++ src/pages/project.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../Css/Project.css";
+
+export default function Project() {
+  return (
+    <div className="_project">
+      <h3>project's</h3>
+      <div className="_projectContainter">
+        <div className="_projecttitle">
+          <h4>react video chat app </h4>
+        </div>
+        <div className="_projectdetails"></div>
+      </div>
+    </div>
+  );
+}
